211a67d184eca14bdcef9ab3be6deaefaeadffc9
marsianin88 <marcin8wiech@gmail.com>
2018-11-25 10:39:27 +0000

    map is updated

--- public/assets/js/Heatmap/heatmap.js-master/examples/googlemaps-heatmap/invoke.js
+++ public/assets/js/Heatmap/heatmap.js-master/examples/googlemaps-heatmap/invoke.js
@@ -1,34 +1,43 @@
 
 var testData = {
     max: 1000,
-    data: [{lat: 51.77, lng: -1.24, count: 10}, {lat: 52.77, lng: -1.8, count: 2},{lat: 53.77, lng: -1.6, count: 10}]
+    data: []
   };
-
-
-
-//make testData dependant on the posts
 
 
 $.ajax({
     type: 'GET',
     url: 'http://localhost:3000/api/posts',
-    success: msg => msg.forEach(obj => buildCard(obj)),
+    success: msg => msg.forEach(obj => (getLatLong(obj))),
     error: err => alert(JSON.stringify(err))
 });
 
-function buildCard(response) {
-    const {place} = response;
-    const { lat, long } = place
 
-    if(lat === undefined || long === undefined){
-        return null;
+//make testData dependant on the posts {lat: 51.77, lng: -1.24, count: 1}, {lat: 52.77, lng: -1.8, count: 1},{lat: 53.77, lng: -1.6, count: 1}
+
+
+
+
+function getLatLong(response) {
+    const {place, danger} = response;
+
+
+
+    if(place  === undefined){
+        return {}
+    }
+    else if(place.location  === undefined && danger){
+        return {}
     }
     else{
-        testData.data.push({lat: lat, lng: long, count: 5})
+        testData.data.push({lat: place.location.latitude, lng: place.location.longitude, count: 100});
+        return {lat: place.location.latitude, lng: place.location.longitude, count: 100}
     }
 
 }
 
-setData(testData);
+
+setTimeout(function(){setData(testData)}, 5000);
+
 
 
