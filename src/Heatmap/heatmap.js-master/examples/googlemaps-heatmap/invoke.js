f433ba1e3202534c449213f18733a9fe39c4f17c
marsianin88 <marcin8wiech@gmail.com>
2018-11-25 07:05:18 +0000

    insert map

---
+++ src/Heatmap/heatmap.js-master/examples/googlemaps-heatmap/invoke.js
@@ -0,0 +1,32 @@
+
+var testData = {
+    max: 1000,
+    data: [{lat: 51.77, lng: -1.24, count: 10}, {lat: 52.77, lng: -1.8, count: 2},{lat: 53.77, lng: -1.6, count: 10},]
+  }
+
+setData(testData);
+
+//make testData dependant on the posts
+/*
+
+$.ajax({
+    type: 'GET',
+    url: 'http://localhost:3000/api/posts',
+    success: msg => msg.forEach(obj => buildCard(obj)),
+    error: err => alert(JSON.stringify(err))
+});
+
+function buildCard(response) {
+    const {latitude, longitude} = response;
+
+    if(latitude === undefined || longitude === undefined){
+        return null;
+    }
+    else{
+        testData.data.push({lat: latitude, lng: longitude, count: 5})
+    }
+
+}
+
+
+*/
--- src/Heatmap/invoke.js
+++
@@ -1,7 +0,0 @@
-
-var testData = {
-    max: 1000,
-    data: [{lat: 51.77, lng: -1.24, count: 10}, {lat: 52.77, lng: -1.8, count: 2},{lat: 53.77, lng: -1.6, count: 1},]
-  }
-
-setData(testData);
